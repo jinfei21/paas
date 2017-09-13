b252135e11113c6fd84d4afee09de21f081414f5
finix <jinfei21@users.noreply.github.com>
2017-09-13 09:52:14 +0800

    Update index.js

--- paas-scheduler/front/src/api/index.js
+++ paas-scheduler/front/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import services from './services'
 
 axios.defaults.timeout = 5000;
-axios.defaults.baseURL = 'http://172.17.2.161:8089';
+axios.defaults.baseURL = 'http://localhost:8089';
 
 
 
